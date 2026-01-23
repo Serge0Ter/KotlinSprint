9a7c7e2417a919f2504c0146e9dedea686ce8d7e
Tereschin_SV <tereshin_ser@>
2026-01-23 14:30:14 +0300

    KS-1-2 task execution

--- src/main/kotlin/lesson_1/Lesson1_task3.kt
+++ src/main/kotlin/lesson_1/Lesson1_task3.kt
@@ -1,5 +1,16 @@
 package org.example.lesson_1
 
 fun main() {
-    println(" Тестовое решение к уроку 1 задач 3")
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
 }
