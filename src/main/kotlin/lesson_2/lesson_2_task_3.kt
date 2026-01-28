9b588f33595d0f73e12f32f357f79128ae248672
Tereschin_SV <tereshin_ser@>
2026-01-28 10:32:13 +0300

    KS-2-3 task execution

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,5 +1,7 @@
 package org.example.lesson_2
 
+const val minuteInHour = 60
+
 fun main() {
 
     val start = "9:39"
@@ -10,10 +12,10 @@ fun main() {
 }
 
 fun timeAsMinutes(start: String, transitTime: Int): String {
-    val minuteInHour = 60
     val startAsInt = start.split(":").map { it.toInt() }
     val minuteStart = startAsInt[0] * minuteInHour + startAsInt[1]
-    val finishHour = (minuteStart + transitTime) / minuteInHour
-    val finishMinute = (minuteStart + transitTime) % minuteInHour
+    val allMinutes = minuteStart + transitTime
+    val finishHour = allMinutes / minuteInHour
+    val finishMinute = allMinutes % minuteInHour
     return "%02d:%02d".format(finishHour, finishMinute)
 }
