9b28324571b440676b47bca7ff262bce33472f7b
Tereschin_SV <tereshin_ser@>
2026-01-29 14:41:56 +0300

    KS-3-3 task execution

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -2,13 +2,18 @@ package org.example.lesson_3
 
 fun main() {
     val number = 6
-    multiplicationTable(number)
-
-}
-
-fun multiplicationTable(item: Int) {
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    for (number in numbers) {
-        println("$item * $number = ${item * number}")
-    }
+    println(
+        """$number * ${numbers[0]} = ${number * numbers[0]}
+        |$number * ${numbers[1]} = ${number * numbers[1]}
+        |$number * ${numbers[2]} = ${number * numbers[2]}
+        |$number * ${numbers[3]} = ${number * numbers[3]}
+        |$number * ${numbers[4]} = ${number * numbers[4]}
+        |$number * ${numbers[5]} = ${number * numbers[5]}
+        |$number * ${numbers[6]} = ${number * numbers[6]}
+        |$number * ${numbers[7]} = ${number * numbers[7]}
+        |$number * ${numbers[8]} = ${number * numbers[8]}""".trimMargin()
+    )
+
 }
+
